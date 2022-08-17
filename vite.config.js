f6c6c47c471eca5dbbb48e49227fec2ec59a96eb
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2022-08-17 10:05:10 -0300

    feat: Adiciona configurações de compatibilidade do vue 3 para vue 2

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,24 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+          vue: '@vue/compat'
+        }
+      },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
         vue({
             template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
+                compilerOptions: {
+                    compatConfig: {
+                        MODE: 2
+                    }
+                }
+            }
         }),
     ],
 });
