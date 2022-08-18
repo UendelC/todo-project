7320025ce14dfc2c5aa49ea82fa140355adb1169
Lucas Nascimento <lucas.barbosa.nas@gmail.com>
2022-08-18 14:08:29 -0300

    feat: Adiciona sidesheet de detalhamento de todo

--- resources/js/pages/TodoList.vue
+++ resources/js/pages/TodoList.vue
@@ -3,42 +3,65 @@
         <h1 class="list__title">
             ToDo List
         </h1>
-        <div class="list__content">
-            <todo
-                class="list__item"
-                v-for="item in todoList"
-                :key="item.description"
-                :item="item"
-            />
-        </div>
-        <div class="list__button">
-            <cds-button
-                variant="blue"
-                size="lg"
-                text="Adicionar novo"
-                @click="handleAdd"
-            />
-        </div>
+        <show-request-provider
+            v-slot="{ loading, failed }"
+            immediate
+            :service="getTodos"
+            @success="successHandler"
+        >
+            <div v-if="loading">
+                <cds-spinner
+                    size="lg"
+                    variant="blue"
+                />
+            </div>
+            <div v-else-if="failed || todoList.length === 0">
+                <cds-empty-state
+                    image="https://cdn-icons-png.flaticon.com/128/4076/4076419.png"
+                    image-description="Imagem de empty state"
+                    title="Não tem nada aqui ainda!"
+                    text="Para sair dessa situação de empty state, realize a ação abaixo"
+                    action-button-text="Adicionar novo"
+                    @action-button-click="handleAdd"
+                />
+            </div>
+            <div v-else>
+                <div class="list__button">
+                    <cds-button
+                        variant="blue"
+                        size="md"
+                        text="Adicionar nova tarefa"
+                        @click="handleAdd"
+                    />
+                </div>
+                <todo
+                    class="list__item"
+                    v-for="item in todoList"
+                    :key="item.id"
+                    :item="item"
+                    @click="handleTodoClick"
+                />
+            </div>
+        </show-request-provider>
+        <cds-side-sheet
+            v-model="showSideSheet"
+            position="left"
+        >
+            <div class="side-sheet">
+                <div class="side-sheet__title">
+                    {{ selectedTodo.title }}
+                </div>
+                <div class="side-sheet__description">
+                    {{ selectedTodo.description }}
+                </div>
+            </div>
+        </cds-side-sheet>
     </div>
 </template>
 
 <script>
 import Todo from '../components/Todo.vue';
-
-const todoList = [
-    {
-        title: 'Todo 1',
-        description: 'Todo 1 é muito bom',
-    },
-    {
-        title: 'Todo 2',
-        description: 'Todo 2 é muito bom',
-    },
-    {
-        title: 'Todo 3',
-        description: 'Todo 3 é muito bom',
-    },
-];
+import { getTodos } from '../services/todo';
 
 export default {
     components: {
@@ -47,14 +70,27 @@ export default {
 
     data() {
         return {
-            todoList,
+            todoList: [],
+            showSideSheet: false,
+            selectedTodo: {},
         };
     },
 
     methods: {
+        getTodos,
+
         handleAdd() {
-            console.log('oi');
+            this.$router.push({ name: 'create-todo' });
         },
+
+        successHandler(response) {
+            this.todoList = JSON.parse(JSON.stringify(response)).data;
+        },
+
+        handleTodoClick(todo) {
+            this.selectedTodo = todo;
+            this.showSideSheet = true;
+        }
     },
 };
 </script>
@@ -69,6 +105,7 @@ export default {
 
     &__title {
         @include heading-1;
+        margin: my(6);
     }
 
     &__item {
@@ -76,7 +113,20 @@ export default {
     }
 
     &__button {
-        margin: mt(2);
+        margin: my(6);
+        display: flex;
+        justify-content: flex-start;
+    }
+}
+
+.side-sheet {
+    padding: pa(4);
+
+    &__title {
+        @include heading-3;
+    }
+    &__description {
+        @include body-1;
     }
 }
 </style>
